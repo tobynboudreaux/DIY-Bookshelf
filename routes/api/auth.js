25fa35b761eee096bb957b978629c40623425776
tobynboudreaux <toby.boudreaux11@gmail.com>
2020-08-25 11:16:13 -0400

    added login route for user and tested

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -21,4 +21,64 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
+// @route           POST api/auth
+// @description     Login/authenticate User
+// @access          Publi
+router.post(
+  "/",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // Check if user exists
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        // Invalid Credentials to prevent data seizure
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // Match User password
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 module.exports = router;
