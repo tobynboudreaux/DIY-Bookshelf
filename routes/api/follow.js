9c700a399277fe757cc6dbdf7001d1eacf915dcb
tobynboudreaux <toby.boudreaux11@gmail.com>
2020-08-26 10:06:16 -0400

    added follow routes and model so User can Follow/Unfollow

--- routes/api/follow.js
+++ routes/api/follow.js
@@ -4,30 +4,52 @@ const { check, validationResult } = require("express-validator");
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
 const Follow = require("../../models/Follow");
-const { db } = require("../../models/User");
 
 // @route           POST api/follow
 // @description     Follow another User
 // @access          Private
 router.post("/user/:id", [auth], async (req, res) => {
   try {
-    const follower = await User.findById(req.user.id).select("-password");
-    const followee = await User.findById(req.params.id).select("-password");
+    const follower = req.user.id;
+    const followee = req.params.id;
 
-    let follow = new Follow({
+    const last = await Follow.findOne({
       follower: follower,
       followee: followee,
     });
 
-    if (follow) {
-      return res
-        .status(400)
-        .json({ errors: [{ msg: "Already following User" }] });
+    if (last) {
+      return res.send("Already following User");
     }
+
+    const follow = new Follow({
+      follower: follower,
+      followee: followee,
+    });
 
     follow.save();
 
-    res.json(follow);
+    return res.send(follow);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route           DELETE api/follow/:id
+// @description     Unfollow a User
+// @access          Private
+router.delete("/unfollow/:id", [auth], async (req, res) => {
+  try {
+    const follower = req.user.id;
+    const followee = req.params.id;
+
+    const follow = await Follow.findOneAndRemove({
+      follower: follower,
+      followee: followee,
+    });
+
+    res.send(follow);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
@@ -38,18 +60,50 @@ router.post("/user/:id", [auth], async (req, res) => {
 // @description     Get all followers
 // @access          Private
 router.get("/followers", [auth], async (req, res) => {
-  const user = await Follow.find({ followee: req.user.id });
+  try {
+    const follow = await Follow.find({ followee: req.user.id });
 
-  res.json(user);
+    const followers = await follow.map((value) => {
+      return value.follower;
+    });
+
+    res.json(followers);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route           GET api/follow/followees
 // @description     Get all followers
 // @access          Private
 router.get("/followees", [auth], async (req, res) => {
-  const user = await Follow.find({ follower: req.user.id });
+  try {
+    const follow = await Follow.find({ follower: req.user.id });
 
-  res.json(user);
+    const followees = await follow.map((value) => {
+      return value.followee;
+    });
+
+    res.json(followees);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.get("/follow", [auth], async (req, res) => {
+  try {
+    const id = req.user.id;
+
+    const user = await User.findOne({ _id: id });
+
+    user.populate([{ path: "following" }, { path: "followers" }]);
+
+    return res.json(user);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
