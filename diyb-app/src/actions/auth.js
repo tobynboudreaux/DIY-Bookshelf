ef87aeb1b210f61a9f69482465339e714848eda8
tobynboudreaux <tobynboudreaux@gmail.com>
2020-09-26 12:59:33 -0500

    ...

--- diyb-app/src/actions/auth.js
+++ diyb-app/src/actions/auth.js
@@ -34,8 +34,6 @@ export const loadUser = () => async (dispatch) => {
 
 // Register User
 export const register = ({ name, email, password }) => async (dispatch) => {
-  await dispatch({ type: CLEAR_PROFILE });
-
   const body = JSON.stringify({ name, email, password });
 
   try {
@@ -61,8 +59,6 @@ export const register = ({ name, email, password }) => async (dispatch) => {
 
 // Login user
 export const login = (email, password) => async (dispatch) => {
-  await dispatch({ type: CLEAR_PROFILE });
-
   const body = JSON.stringify({ email, password });
 
   try {
