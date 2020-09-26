bf29b9dfbe4224853bdd111106ec57f1d15be656
tobynboudreaux <tobynboudreaux@gmail.com>
2020-09-26 12:30:54 -0500

    ...

--- diyb-app/src/store.js
+++ diyb-app/src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { setAuthToken } from "./utils/setAuthToken";
 
 const initialState = {};
 
