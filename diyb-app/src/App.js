b1aa8d897b6987c2270621d5245567fe1abe6efe
tobynboudreaux <tobynboudreaux@gmail.com>
2020-09-26 11:25:58 -0500

    redeploy

--- diyb-app/src/App.js
+++ diyb-app/src/App.js
@@ -10,7 +10,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
-
+//redeploy
 import "./App.css";
 
 if (localStorage.token) {
