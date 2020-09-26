38223201c8733dcd368a9bf39555cd7328dc1e6f
tobynboudreaux <tobynboudreaux@gmail.com>
2020-09-26 11:22:15 -0500

    ...

--- diyb-app/src/App.js
+++ diyb-app/src/App.js
@@ -17,9 +17,17 @@ if (localStorage.token) {
 
 const App = () => {
   useEffect(() => {
+    // check for token in LS
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
     store.dispatch(loadUser());
-  }, []);
 
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener("storage", () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
   return (
     <Provider store={store}>
       <Router>
