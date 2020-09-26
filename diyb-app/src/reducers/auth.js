5e4e7a89e330c785961495009b48e02377c7f380
tobynboudreaux <tobynboudreaux@gmail.com>
2020-09-26 11:36:57 -0500

    ...

--- diyb-app/src/reducers/auth.js
+++ diyb-app/src/reducers/auth.js
@@ -1,10 +1,10 @@
 import {
   REGISTER_SUCCESS,
-  REGISTER_FAIL,
+  // REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL,
+  // LOGIN_FAIL,
   LOGOUT,
   ACCOUNT_DELETED,
   FOLLOW_USER,
@@ -32,24 +32,21 @@ export default function (state = initialState, action) {
       };
     case REGISTER_SUCCESS:
     case LOGIN_SUCCESS:
-      localStorage.setItem("token", payload.token);
       return {
         ...state,
         ...payload,
         isAuthenticated: true,
         loading: false,
       };
-    case REGISTER_FAIL:
     case AUTH_ERROR:
-    case LOGIN_FAIL:
     case LOGOUT:
     case ACCOUNT_DELETED:
-      localStorage.removeItem("token");
       return {
         ...state,
         token: null,
         isAuthenticated: false,
         loading: false,
+        user: null,
       };
     default:
       return state;
